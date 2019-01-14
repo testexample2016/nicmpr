f4f1526251ca901225385a11844df3856c628b60
testexample2016 <testexample2016@gmail.com>
2019-01-14 05:41:30 +0530

    14/01/209@5:41am
    Eager loading & Modified Preview

--- app/Http/Controllers/GenerateController.php
+++ app/Http/Controllers/GenerateController.php
@@ -21,13 +21,131 @@ class GenerateController extends Controller
       
       public function preview(){
 
-    	 $employees = User::all();
+    	 $employees = User::with('projects','newprojects','inaugurations','trainings','awards','initiatives','reviews')->get();
+
 
     	 $mprdurationstatus = mprdurationcheck();
 
     	 $date = createdate();
 
-    	return view('generate.preview',compact('employees','mprdurationstatus','date'));
+        $review_generate = array();
+
+      $initiative_generate = array();
+
+       $award_generate = array();
+
+      $training_generate = array();
+
+      $inauguration_generate  =array();
+
+      $newproject_state_generate = array();
+
+      $newproject_central_generate =array();
+
+      $project_state_generate = array();
+
+
+    
+
+
+      foreach($employees as $employee){
+
+        foreach ($employee->reviews as $review) {
+
+          if($review->year_month == $date){
+
+            $review_generate[] = $review;
+          }
+
+          
+        }
+
+        // $progressBar->advance();
+
+        foreach ($employee->initiatives as $initiative) {
+
+          if($initiative->year_month == $date){
+
+            $initiative_generate[] = $initiative;
+          }
+        }
+
+
+            foreach ($employee->awards as $award) {
+
+          if($award->year_month == $date){
+
+            $award_generate[] = $award;
+          }
+
+      }
+
+
+        foreach ($employee->trainings as $training) {
+
+          if($training->year_month == $date){
+
+            $training_generate[] = $training;
+          }
+
+      }
+
+
+      foreach ($employee->inaugurations as $inauguration) {
+
+          if($inauguration->year_month == $date){
+
+            $inauguration_generate[] = $inauguration;
+          }
+
+      }
+
+      foreach ($employee->newprojects as $newproject) {
+
+
+          if($newproject->year_month == $date){
+
+            if($newproject->central_state){
+
+               $newproject_state_generate[] = $newproject;
+
+            }
+
+            else{
+
+               $newproject_central_generate[] = $newproject;
+
+            }
+
+           
+          }
+
+      }
+
+
+      foreach ($employee->projects as $project) {
+
+          
+
+            if($project->central_state){
+
+              $project_state_generate[] = $project;
+            }
+
+          else{
+
+            $project_central_generate[] = $project;
+          }
+
+      }
+
+     
+
+
+
+}
+
+    	return view('generate.preview',compact('employees','mprdurationstatus','date','review_generate','initiative_generate','award_generate','training_generate','inauguration_generate','newproject_state_generate','newproject_central_generate','project_state_generate','project_central_generate'));
 
 
     }
@@ -41,7 +159,7 @@ class GenerateController extends Controller
 
 
 
-      $employees = User::all();
+      $employees = User::with('projects','newprojects','inaugurations','trainings','awards','initiatives','reviews')->get();
 
 
       $mprdurationstatus = mprdurationcheck();
